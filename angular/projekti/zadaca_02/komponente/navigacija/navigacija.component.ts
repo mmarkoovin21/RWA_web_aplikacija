47274a515fdae99f707aa545fa15dfb5217521fa
Magdalena Markovinvić <mmarkovin21@foi.hr>
2023-12-10 00:29:51 +0100

    Popravljena angular navigacija tako da se ne učitava stranica svaki puta

--- angular/projekti/zadaca_02/komponente/navigacija/navigacija.component.ts
+++ angular/projekti/zadaca_02/komponente/navigacija/navigacija.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigacija',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './navigacija.component.html',
   styleUrl: './navigacija.component.scss'
 })

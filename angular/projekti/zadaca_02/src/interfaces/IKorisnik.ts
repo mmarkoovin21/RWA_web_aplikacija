195a2fac3bc1abd9e85a2c8a16396da289b53dc8
Magdalena Markovinvić <mmarkovin21@foi.hr>
2023-12-18 23:51:20 +0100

    Dodana sučelja za TMDB Serije, Sezone, Korisnike i Seriju iz favorita

---
+++ angular/projekti/zadaca_02/src/interfaces/IKorisnik.ts
@@ -0,0 +1,11 @@
+export interface IKorisnik{
+    id: number;
+    ime: string;
+    prezime: string;
+    adresa: string;
+    spol: string;
+    zvanje: string;
+    email: string;
+    korime: string;
+    uloga_korisnika_id: number;
+}

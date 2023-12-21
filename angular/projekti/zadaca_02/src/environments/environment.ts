6f08ea3940fab397b44f455b19355aed14e82112
Magdalena Markovinvić <mmarkovin21@foi.hr>
2023-12-21 22:10:10 +0100

    Promjenjena rest putanja

--- angular/projekti/zadaca_02/src/environments/environment.ts
+++ angular/projekti/zadaca_02/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     path: "http://localhost:",
-    restServis: 'http://spider.foi.hr:12420/api/',
+    restServis: 'http://localhost:12420/api/',
     posteriPutanja: "https://image.tmdb.org/t/p/original"
 };

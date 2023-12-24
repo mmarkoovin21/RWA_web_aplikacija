9e94af646e7c5e594122f89da163f1a6618f9afa
Magdalena Markovinvić <mmarkovin21@foi.hr>
2023-12-24 14:04:19 +0100

    Dodan environment.prod.ts, projekt se sada pravilno build-a.

---
+++ angular/projekti/zadaca_02/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    path: "http://localhost:",
+    restServis: 'http://localhost:12420',
+    posteriPutanja: "https://image.tmdb.org/t/p/original"
+};

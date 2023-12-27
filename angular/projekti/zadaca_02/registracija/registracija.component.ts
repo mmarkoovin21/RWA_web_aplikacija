03bbf92bf1c276a7fe7322eff7e45c1dd99110d3
Magdalena Markovinvić <mmarkovin21@foi.hr>
2023-12-27 18:47:43 +0100

    Djelomično napravljena registracija.
    Korisnici se spremaju u bazu ali bez probjere jwt-a.

--- angular/projekti/zadaca_02/registracija/registracija.component.ts
+++ angular/projekti/zadaca_02/registracija/registracija.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '../src/environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-registracija',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
     styleUrl: './registracija.component.scss',
 })
 export class RegistracijaComponent {
+    private restServis = environment.restServis;
+    ime: string = '';
+    prezime: string = '';
+    adresa: string = '';
+    musko: string = '';
+    zensko: string = '';
+    spol: string = '';
+    zvanje: string = '';
+    lozinka: string = '';
+    email: string = '';
+    korime: string = '';
+    constructor(
+        private router : Router
+    ){
+}
 
+    async submit() {
+        if(this.musko != ''){
+            this.spol = this.musko;
+        }else if(this.zensko != ''){
+            this.spol = this.zensko;
+        }
+        let tijelo = {
+            ime: this.ime,
+            prezime: this.prezime,
+            adresa: this.adresa,
+            spol: this.spol,
+            zvanje: this.zvanje,
+            lozinka: this.lozinka,
+            email: this.email,
+            korime: this.korime
+        }
+        let zaglavlje = new Headers();
+        zaglavlje.set("Content-Type", "application/json");
+
+        let odgovorRegistriraj = await fetch("/baza/korisnici", {
+            method: "POST",
+            headers: zaglavlje,
+            body: JSON.stringify(tijelo)
+        });
+        if(odgovorRegistriraj.status == 201){
+            this.router.navigate(["pocetna"]);
+        }
+    }
 }
